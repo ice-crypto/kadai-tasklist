34308fe211fdf0ba11a9b3a8758693c8f8c201d9
EC2 Default User <ec2-user@ip-172-31-82-170.ec2.internal>
2020-04-22 10:01:33 +0000

    model　user has many tasks belonged to . １対多構築済み

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  post 'sessions/create'
+  delete 'sessions/destroy'
+  get 'users/index'
+  post 'users/create'
+  get 'users/new'
+  
   resources :tasks
 end
